dd016834a66586dea7c338dc106e8d067060ef85
stefan binoj <stefan.binoj.007@gmail.com>
2025-11-29 22:03:24 +0530

    added validation for comprehension based

--- app/routers/validator.py
+++ app/routers/validator.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, HTTPException
 
-from app.schemas.mongo_models import GenerationLog, QuestionLog
-from app.schemas.input_schema import QuestionReqPara
+from app.schemas.mongo_models import ComprehensionLog, GenerationLog, QuestionLog
+from app.schemas.input_schema import ComprehensionReqPara, QuestionReqPara
 from app.schemas.output_schema import QuestionItem, OptionLabel
 from app.helpers.chroma_helper import search_similar_questions
 from app.helpers.validation_helper import validate_questions
@@ -47,4 +47,35 @@ async def validate_question(question_id: str):
 
 @router.post("/validate-passage")
 async def validate_passage(question_id: str):
-    return {"status": "To be implemented"}
+    log = await ComprehensionLog.find_one({"questions.chroma_id": question_id})
+
+    if not log:
+        raise HTTPException(status_code=404, detail="Question not found for validation")
+
+    res: QuestionLog = [q for q in log.questions if q.chroma_id == question_id][0]
+    question = QuestionItem(
+        question=res.question,
+        options=res.options,
+        correct_option=OptionLabel(res.correct_option),
+        explanation=res.explanation,
+    )
+
+    req: ComprehensionReqPara = log.request
+
+    print(f"\n\n    --->Validating single question {question.question}")
+
+    similar_questions = await search_similar_questions(
+        question=question, subject=req.subject, topic=req.topic, top_k=3
+    )
+
+    check_validation , validation_time = await validate_questions(
+        req, question, similar_questions[1:], add_to_db=False
+    )
+    return {
+        "_id": res.chroma_id,
+        "validation_score": check_validation.validation_result.score,
+        "duplication_chance": check_validation.validation_result.duplication_chance,
+        "issues": check_validation.validation_result.issues,
+        "similar_questions": similar_questions,
+        "validation_time": validation_time,
+    }
